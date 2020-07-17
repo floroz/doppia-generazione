fd6dacc931079f22b86db144aade4c26fba2273b
Daniele <tortora87@gmail.com>
2020-07-17 16:28:42 +0100

    setup layout and sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "./layout.css"
+import "../styles.scss"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
