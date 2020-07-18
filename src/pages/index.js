94ef28b40a371e2e133106ef7d24c22aef59629b
Daniele <tortora87@gmail.com>
2020-07-18 10:17:52 +0100

    setup initial page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./index.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Work in Progress</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <img src={require("../images/doppiag-logo.png")} className="logo" />
+      <h1 style={{ color: "var(--secondary-color)" }} className="title">
+        @doppiagenerazione
+      </h1>
+      <img
+        src={require("../images/doppiag-hero.jpg")}
+        style={{ maxWidth: 450 }}
+        className="hero"
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
