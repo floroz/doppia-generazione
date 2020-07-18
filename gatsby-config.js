01bb0243083942114aa481871f680aa2b5b79806
Daniele <tortora87@gmail.com>
2020-07-18 10:17:59 +0100

    created index and 404 pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Doppia Generazione`,
+    description: `nasce con l'obiettivo di colmare un gap informativo e generazionale nel mondo dell'imprenditoria italiana, focalizzandosi su modelli di business duraturi e radicati ma, allo stesso tempo, sull'affermarsi di realta' imprenditoriali 2.0, vogliose di cambiamento e nuove opportunità`,
+    author: `@floroz87`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/doppiag-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,19 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "../styles.scss"
+import "normalize.css"
 import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
